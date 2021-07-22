838310b6b25d45c6f9bf64084557ac9b22fb3700
Isisaurus <dianavitanyi@gmail.com>
2021-07-22 15:31:07 +0200

    Basic form setup

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import Layout from './../components/Layout';
+import { Typography } from '@material-ui/core';
 
 function contact() {
-  return <Layout>contact</Layout>;
+  return (
+    <Layout>
+      <Typography variant="h1">Contact me</Typography>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <div>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </div>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Layout>
+  );
 }
 
 export default contact;
