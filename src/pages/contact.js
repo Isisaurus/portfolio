863d6de719c90523c295308eca976f6def6dcdbf
Isisaurus <dianavitanyi@gmail.com>
2021-07-22 16:22:08 +0200

    Styled form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 import Layout from './../components/Layout';
-import { Typography } from '@material-ui/core';
+import {
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Container,
+} from '@material-ui/core';
 import { navigate } from 'gatsby-link';
+
+import SendIcon from '@material-ui/icons/Send';
 
 function Contact() {
   const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  const handleChange = e => {
+  const handleChangeEmail = e => {
     e.preventDefault();
     setEmail(e.target.value);
+  };
+  const handleChangeMessage = e => {
+    e.preventDefault();
+    setMessage(e.target.value);
     console.log(e.target.value);
   };
   function encode(data) {
@@ -25,6 +38,7 @@ function Contact() {
       body: encode({
         'form-name': event.target.getAttribute('name'),
         email,
+        message,
       }),
     })
       .then(() => navigate('/'))
@@ -33,20 +47,70 @@ function Contact() {
 
   return (
     <Layout>
-      <Typography variant="h1">Contact me</Typography>
-      <form
-        data-netlify="true"
-        name="contact"
-        method="post"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <label>
-          Your email
-          <input name="email" type="text" onChange={handleChange} />
-        </label>
-        <input type="submit" />
-      </form>
+      <Container style={{ minHeight: '90vh' }}>
+        <Typography variant="h1">Contact me</Typography>
+        <Grid container>
+          <Grid item sm={12} md={6}>
+            <Typography>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              mollitia placeat animi officiis reprehenderit, quia corrupti
+              officia vero inventore explicabo.
+            </Typography>
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <Container>
+              <form
+                data-netlify="true"
+                name="contact"
+                method="post"
+                onSubmit={handleSubmit}
+              >
+                <input type="hidden" name="form-name" value="contact" />
+
+                <TextField
+                  name="email"
+                  type="email"
+                  label="Your email"
+                  onChange={handleChangeEmail}
+                  required
+                  color="primary"
+                  variant="outlined"
+                  size="medium"
+                  fullWidth
+                  placeholder="example@email.com"
+                  margin="normal"
+                />
+                <TextField
+                  name="message"
+                  type="text"
+                  label="Your message"
+                  onChange={handleChangeMessage}
+                  required
+                  color="primary"
+                  variant="outlined"
+                  size="medium"
+                  fullWidth
+                  multiline
+                  rows={6}
+                  placeholder="Your message..."
+                  margin="normal"
+                />
+                <Button
+                  startIcon={<SendIcon />}
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  disableElevation
+                  style={{ marginTop: '1rem' }}
+                >
+                  Send
+                </Button>
+              </form>
+            </Container>
+          </Grid>
+        </Grid>
+      </Container>
     </Layout>
   );
 }
