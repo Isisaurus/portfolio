dc0bd42f8b6b1dcab81bdb2202e0f1b4e50e7807
Isisaurus <dianavitanyi@gmail.com>
2021-07-23 15:21:58 +0200

    Linkedin icon from material ui.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,11 +9,11 @@ import {
   Container,
   makeStyles,
   List,
-  SvgIcon,
 } from '@material-ui/core';
 import { Link } from 'gatsby-theme-material-ui';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import SendIcon from '@material-ui/icons/Send';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 import EmailIcon from '@material-ui/icons/Email';
 
@@ -207,23 +207,7 @@ function Contact() {
                     github/Isisaurus
                   </Link>
                 </Button>
-                <Button
-                  className={classes.btn}
-                  startIcon={
-                    <SvgIcon>
-                      <svg
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        height="25"
-                        viewBox="0 0 22 22"
-                      >
-                        <title>linkedin-with-circle</title>
-                        <path d="M10 0.4c-5.302 0-9.6 4.298-9.6 9.6s4.298 9.6 9.6 9.6 9.6-4.298 9.6-9.6-4.298-9.6-9.6-9.6zM7.65 13.979h-1.944v-6.256h1.944v6.256zM6.666 6.955c-0.614 0-1.011-0.435-1.011-0.973 0-0.549 0.409-0.971 1.036-0.971s1.011 0.422 1.023 0.971c0 0.538-0.396 0.973-1.048 0.973zM14.75 13.979h-1.944v-3.467c0-0.807-0.282-1.355-0.985-1.355-0.537 0-0.856 0.371-0.997 0.728-0.052 0.127-0.065 0.307-0.065 0.486v3.607h-1.945v-4.26c0-0.781-0.025-1.434-0.051-1.996h1.689l0.089 0.869h0.039c0.256-0.408 0.883-1.010 1.932-1.010 1.279 0 2.238 0.857 2.238 2.699v3.699z"></path>
-                      </svg>
-                    </SvgIcon>
-                  }
-                >
+                <Button className={classes.btn} startIcon={<LinkedInIcon />}>
                   <Link
                     to="https://www.linkedin.com/in/diana-vitanyi-49211a15a/"
                     target="__blank"
