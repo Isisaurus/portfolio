9f9dba39ba999bc701801cc9973537582314198e
Isisaurus <dianavitanyi@gmail.com>
2021-07-23 15:57:05 +0200

    About page placeholder.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Layout from './../components/Layout';
+import { Container, Typography, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    margin: '4rem 0',
+    [theme.breakpoints.down('sm')]: {
+      margin: '2rem 0',
+    },
+  },
+}));
 
 const About = () => {
+  const classes = useStyles();
   return (
     <Layout>
-      <h1>About</h1>
+      <Container className={classes.container}>
+        <Typography variant="h5">
+          {' '}
+          Sorry. This page is under construction.
+        </Typography>
+      </Container>
     </Layout>
   );
 };
